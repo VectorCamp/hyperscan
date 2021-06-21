ad8799075d36fda93f048dfe6c246bc9fee69fd9
apostolos <apostolos.tapsas@vectorcamp.gr>
2021-06-21 19:42:09 +0300

    SuperVectro-unit-tests

---
+++ build/CMakeFiles/hs.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/vcamp/Vectrocamp/vectorscan/src/hs_valid_platform.c" "/home/vcamp/Vectrocamp/vectorscan/build/CMakeFiles/hs.dir/src/hs_valid_platform.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/hs_version.c" "/home/vcamp/Vectrocamp/vectorscan/build/CMakeFiles/hs.dir/src/hs_version.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "../src"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
