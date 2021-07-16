bb5387a6385b06a47b86b6ad01d43904748caafa
apostolos <apostolos.tapsas@vectorcamp.gr>
2021-07-16 10:40:51 +0300

    SuperVector AVX512 implementations

---
+++ build-avx512/CMakeFiles/hs_exec.dir/DependInfo.cmake
@@ -0,0 +1,91 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/vcamp/Vectrocamp/vectorscan/src/alloc.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/alloc.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/crc32.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/crc32.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/database.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/database.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/fdr/fdr.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/fdr/fdr.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/fdr/teddy.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/fdr/teddy.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/fdr/teddy_avx2.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/fdr/teddy_avx2.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/hwlm/hwlm.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/hwlm/hwlm.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/accel.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/accel.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/castle.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/castle.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/gough.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/gough.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/lbr.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/lbr.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/limex_64.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/limex_64.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/limex_accel.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/limex_accel.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/limex_native.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/limex_native.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/limex_simd128.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/limex_simd128.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/limex_simd256.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/limex_simd256.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/limex_simd384.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/limex_simd384.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/limex_simd512.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/limex_simd512.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/mcclellan.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/mcclellan.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/mcsheng.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/mcsheng.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/mcsheng_data.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/mcsheng_data.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/mpv.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/mpv.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/nfa_api_dispatch.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/nfa_api_dispatch.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/repeat.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/repeat.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/sheng.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/sheng.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/tamarama.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/tamarama.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/rose/block.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/rose/block.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/rose/catchup.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/rose/catchup.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/rose/init.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/rose/init.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/rose/match.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/rose/match.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/rose/program_runtime.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/rose/program_runtime.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/rose/stream.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/rose/stream.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/runtime.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/runtime.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/scratch.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/scratch.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/som/som_runtime.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/som/som_runtime.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/som/som_stream.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/som/som_stream.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/stream_compress.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/stream_compress.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/util/arch/x86/cpuid_flags.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/util/arch/x86/cpuid_flags.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/util/arch/x86/masked_move.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/util/arch/x86/masked_move.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/util/multibit.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/util/multibit.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/util/simd_utils.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/util/simd_utils.c.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/util/state_compress.c" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/util/state_compress.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "../src"
+  "../include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vcamp/Vectrocamp/vectorscan/src/hwlm/noodle_engine.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/hwlm/noodle_engine.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/shufti.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/shufti.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/nfa/truffle.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/nfa/truffle.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/src/util/supervector/arch/x86/impl.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs_exec.dir/src/util/supervector/arch/x86/impl.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "../src"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-avx512/unit/CMakeFiles/unit-hyperscan.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vcamp/Vectrocamp/vectorscan/unit/gtest/gtest-all.cc" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/gtest/gtest-all.cc.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/allocators.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/allocators.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/arg_checks.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/arg_checks.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/bad_patterns.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/bad_patterns.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/behaviour.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/behaviour.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/expr_info.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/expr_info.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/extparam.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/extparam.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/identical.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/identical.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/literals.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/literals.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/logical_combination.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/logical_combination.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/main.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/main.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/multi.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/multi.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/order.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/order.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/scratch_in_use.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/scratch_in_use.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/scratch_op.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/scratch_op.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/serialize.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/serialize.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/single.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/single.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/som.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/som.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/stream_op.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/stream_op.cpp.o"
+  "/home/vcamp/Vectrocamp/vectorscan/unit/hyperscan/test_util.cpp" "/home/vcamp/Vectrocamp/vectorscan/build-avx512/unit/CMakeFiles/unit-hyperscan.dir/hyperscan/test_util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEBUG"
+  "GTEST_HAS_PTHREAD=0"
+  "SRCDIR=/home/vcamp/Vectrocamp/vectorscan"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "unit"
+  "../src"
+  "."
+  "../"
+  "../unit"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/vcamp/Vectrocamp/vectorscan/build-avx512/CMakeFiles/hs.dir/DependInfo.cmake"
+  "/home/vcamp/Vectrocamp/vectorscan/build-avx512/util/CMakeFiles/expressionutil.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
